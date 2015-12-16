1bf46d55e8266d44fb4990aef3cba68ac2c1e42a
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2015-12-16 13:44:47 +0100

    Clean the structure of T3 spheres and minor code cleanup.

--- src/checksum.h
+++ src/checksum.h
@@ -4,22 +4,22 @@
 char* author0 = "Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>";
 // char* author1 = "Mikolaj Kowalik <kowalik@ifmpan.poznan.pl>";
 char* builder = "Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>";
-char* build = "Mon Dec 14 11:13:10 CET 2015";
+char* build = "Wed Dec 16 13:41:37 CET 2015";
 
-char* fccdcgen_c_SHA1 = "08d44f27ac81f575bad6a087af4a95dcd03c3455";
-char* globals_h_SHA1 = "9d03b0ca40233e8302e150581226befaa7d43475";
+char* fccdcgen_c_SHA1 = "846940f30f6394438595d3dd843ede49052c1c30";
+char* globals_h_SHA1 = "bad502afedc2807024b4b9c4dfef73016fe9733c";
 char* mt19937_h_SHA1 = "08961c2d0bfffac380ab1ee575c0e530c95d762d";
 char* mt19937_64_h_SHA1 = "b442092d46c546522b41f0e9995901f78d6d1d6e";
-char* config_h_SHA1 = "a85e053691656184a13119b0d914f11da7c5e399";
+char* config_h_SHA1 = "8530bea874dbcdcf6b9657fe4b13a723b468b2b4";
 char* data_h_SHA1 = "97b8e7771f418afb51c9628f2eb9dc7f795044d5";
-char* algebra_h_SHA1 = "0671823900f045c89985bde51a754c3768c01ecb";
-char* algebra_c_SHA1 = "b90bfc37e752a4415c98236d877c29ab0410a52f";
+char* algebra_h_SHA1 = "83aadae4004659235538ca44d2276f0aa75e1afd";
+char* algebra_c_SHA1 = "fda03137c135694af31a0873e7b17826a8ce0c47";
 char* initials_h_SHA1 = "0502e6ef92e5328cce4e47e8fa1d487698580c46";
-char* initials_c_SHA1 = "ad4801c61c6b7f3c102238507c1d644bf7356eb1";
+char* initials_c_SHA1 = "9e2596006b50170594b6ffad7018364f6da94cbe";
 char* io_h_SHA1 = "557d08349da4874d21308ed51da55da520ce6702";
-char* io_c_SHA1 = "1ec757449a7e42652d34015cf67a8a5865522236";
-char* utils_h_SHA1 = "26a35b14c337c54eb1f942ad35967e9e4e8ef61c";
-char* utils_c_SHA1 = "5810ceb63a7fcb46f2a5688202d5734224902221";
+char* io_c_SHA1 = "b3c4624da0772e4a7697709fb30d1da9d8e00924";
+char* utils_h_SHA1 = "6c8e1ab7aaaa334257db26d5d957ef0b088f72eb";
+char* utils_c_SHA1 = "7fe5c1d89aec6caa41a9856ec0e33d4ef7e1de78";
 
 #endif
 
