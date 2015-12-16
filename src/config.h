dc4f644442c58d5b5d91ffe945b451083fba11f2
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2015-12-16 13:36:08 +0100

    Update config files. Clean unused options and implement selection of all RNG used.

--- src/config.h
+++ src/config.h
@@ -10,21 +10,22 @@
 // Uncoment the following to save the system's evolution to trajectory files
 #define DATA_VISGL_OUTPUT
 
-// Uncoment the following to export mean square displacement for particles
-// #define PUSH_MSD
+// Uncoment the following to enable debugging output
+// #define DEBUG_MODE
+
+/* 
+ * Pseudo random number generator section
+ * 
+ * NOTE: Pay attention that ONLY ONE of the following three options is active
+ * Any TWO of the following #define's should be ALWAYS COMMENTED OUT
+ */
+// Uncoment the following to use the standard dran48 r.n.g.
+// #define USE_DRAND48
+
+// Uncoment the following to use the 32bit implementation of MT 19937 r.n.g.
+#define USE_32BIT_MT19937
 
 // Uncoment the following to use the 64bit implementation of MT 19937 r.n.g.
 // #define USE_64BIT_MT19937
-
-// Uncoment the following to enable permanent kill mode (target are not
-// recreated at random positions after hit)
-// #define PERMANENT_KILL
-
-// Compile program with capture-the-flag-mode, i.e. targets must reach specified
-// area. This mode assumes permanentKill=1
-// #define CAPTURE_THE_FLAG
-
-// Uncoment the following to enable debugging output
-// #define DEBUG_MODE
 
 #endif
--- src/config.sh
+++ src/config.sh
@@ -9,16 +9,34 @@ else
   sed -i 's/^.*\(#define\ DATA_VISGL_OUTPUT.*$\)/\/\/\ \1/' $cfile
 fi
 
-if [ $use64MT19937 -eq 1 ]; then
-  sed -i 's/\/\/\ \(#define\ USE_64BIT_MT19937.*$\)/\1/' $cfile
-else
-  sed -i 's/^.*\(#define\ USE_64BIT_MT19937.*$\)/\/\/\ \1/' $cfile
-fi
-
 if [ $debugMode -eq 1 ]; then
   sed -i 's/\/\/\ \(#define\ DEBUG_MODE.*$\)/\1/' $cfile
 else
   sed -i 's/^.*\(#define\ DEBUG_MODE.*$\)/\/\/\ \1/' $cfile
+fi
+
+if [ $use64MT19937 -eq 1 ]; then
+  sed -i 's/\/\/\ \(#define\ USE_64BIT_MT19937.*$\)/\1/' $cfile
+  sed -i 's/^.*\(#define\ USE_32BIT_MT19937.*$\)/\/\/\ \1/' $cfile
+  sed -i 's/^.*\(#define\ USE_DRAND48.*$\)/\/\/\ \1/' $cfile
+else
+  sed -i 's/^.*\(#define\ USE_64BIT_MT19937.*$\)/\/\/\ \1/' $cfile
+fi
+
+if [ $use32MT19937 -eq 1 ]; then
+  sed -i 's/\/\/\ \(#define\ USE_32BIT_MT19937.*$\)/\1/' $cfile
+  sed -i 's/^.*\(#define\ USE_64BIT_MT19937.*$\)/\/\/\ \1/' $cfile
+  sed -i 's/^.*\(#define\ USE_DRAND48.*$\)/\/\/\ \1/' $cfile
+else
+  sed -i 's/^.*\(#define\ USE_32BIT_MT19937.*$\)/\/\/\ \1/' $cfile
+fi
+
+if [ $useDRAND48 -eq 1 ] || [ $use32MT19937 -eq 0 -a $use64MT19937 -eq 0 ]; then
+  sed -i 's/\/\/\ \(#define\ USE_DRAND48.*$\)/\1/' $cfile
+  sed -i 's/^.*\(#define\ USE_32BIT_MT19937.*$\)/\/\/\ \1/' $cfile
+  sed -i 's/^.*\(#define\ USE_64BIT_MT19937.*$\)/\/\/\ \1/' $cfile
+else
+  sed -i 's/^.*\(#define\ USE_DRAND48.*$\)/\/\/\ \1/' $cfile
 fi
 
 exit 0
