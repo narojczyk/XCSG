31596dc886fa3556c12fbf4910cbb7630db056a4
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2017-07-31 14:50:57 +0200

    Fix wrong variable names for channel datat in parse_channels()

--- src/initials.c
+++ src/initials.c
@@ -253,14 +253,14 @@ int parse_channels(FILE *file, CHA ch_tab[])
     }else{
     
     // Store offset for the channel 'i'
-    ch_tab[i].offset[0] = c_off[0];
-    ch_tab[i].offset[1] = c_off[1];
-    ch_tab[i].offset[2] = c_off[2];
+    ch_tab[i].os[0] = c_off[0];
+    ch_tab[i].os[1] = c_off[1];
+    ch_tab[i].os[2] = c_off[2];
     
     // Store normal vector for the channel 'i'
-    ch_tab[i].normal[0] = c_nor[0];
-    ch_tab[i].normal[1] = c_nor[1];
-    ch_tab[i].normal[2] = c_nor[2];
+    ch_tab[i].nm[0] = c_nor[0];
+    ch_tab[i].nm[1] = c_nor[1];
+    ch_tab[i].nm[2] = c_nor[2];
     
     // Store radius for the channel 'i'
     ch_tab[i].radius = c_r;
