0706f908df371ab11621887728827c2362ba754c
Jakub W. Narojczyk <narojczyk@ifmpan.poznan.pl>
2019-10-02 11:24:00 +0200

    Drop support for mko dcsgen strutures

--- src/initials.c
+++ src/initials.c
@@ -19,7 +19,7 @@ extern char *prog_name;
 extern unsigned long int i_seed;
 extern int i_edge_fcc_N[3];
 extern int i_ch_layout[3];
-extern int i_load_DC_from_file;
+extern int i_load_DC_from_file; // Obsolete, TBR while ini file structure changes
 extern int i_iDCfrom;
 extern int i_iDCto;
 extern int i_make_channel;
@@ -356,7 +356,7 @@ int parse_config(FILE *file)
   fscanf(file, "%*26c %lu\n",      &i_seed);
   fscanf(file, "%*26c %d %d %d\n", &i_edge_fcc_N[0], &i_edge_fcc_N[1],
          &i_edge_fcc_N[2]);
-  fscanf(file, "%*26c %d\n",       &i_load_DC_from_file);
+  fscanf(file, "%*26c %d\n",       &i_load_DC_from_file); // Obsolete, TBR while ini file structure changes
   fscanf(file, "%*26c %d %d\n",    &i_iDCfrom, &i_iDCto);
   fscanf(file, "%*26c %d\n",       &i_make_channel);
   fscanf(file, "%*26c %d\n",       &i_make_slit);
